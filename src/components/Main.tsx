e92d376851b698332975eeb58697c539ceb85b96
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-04-17 20:47:04 +1000

    Ability to remove cryptos from portfolio both local and backend db, fixed UI bug of add coin button apearing over modal

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,9 +2,10 @@ import { useState, useEffect } from 'react';
 import './Main.css';
 import LoginDisp from './LoginDisp';
 import { auth, db } from './BaseFire';
-import { collection, getDocs, updateDoc, doc } from "firebase/firestore"; 
+import { collection, getDocs, updateDoc, doc, deleteField  } from "firebase/firestore"; 
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import Modal from 'react-modal';
+import {BsTrash} from 'react-icons/bs'
 
 const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
@@ -103,7 +104,12 @@ const Main = () => {
   if (isNeg) {
     amount = -amount
   } 
-  const newCoinAmount = coins[coin] + amount;
+
+  let newCoinAmount = coins[coin] + amount;
+
+  if (newCoinAmount < 0) {
+    newCoinAmount = 0
+  }
 
   // Update Firestore document with new coin amount
   const docRef = doc(db, "users", userEmail);
@@ -131,6 +137,40 @@ const Main = () => {
   setTransAmount(0)
   setIsNeg(false)
   setTransText("Amount In")
+ }
+
+ const [openTrash, setOpenTrash] = useState<boolean>(false)
+ const [deleteCoin, setDeleteCoin] = useState<string>('')
+
+ function delCoin(trashModal:boolean, coin:string) {
+  setOpenTrash(trashModal)
+  console.log("Coin to delete: ", coin)
+  setDeleteCoin(coin)
+ }
+
+ function closeSure() {
+  setOpenTrash(false)
+  setDeleteCoin('')
+ }
+
+ const handleSure = async() => {
+      //Make Function Asynch
+      //Handle removing field from Coins in DB and Local here that macth the deleteCoin state
+      if (!(deleteCoin in coins)) {
+        setOpenTrash(false)
+        return;
+      }
+      // Delete field from Firestore document
+      const docRef = doc(db, "users", userEmail);
+      await updateDoc(docRef, {
+        [`Coins.${deleteCoin}`]: deleteField(),
+      });
+  
+      // Update local state with deleted field
+      const { [deleteCoin]: _, ...newCoins } = coins;
+      setCoins(newCoins);
+      setDeleteCoin("");
+      setOpenTrash(false)
  }
 
     return (
@@ -161,18 +201,22 @@ const Main = () => {
               <h3>Existing Coins</h3>
               <ListGroup className="coin-list mx-auto">
                 {Object.entries(coins).map(([coin, amount]) => (
-                    <ListGroupItem key={coin} className="d-flex justify-content-between align-items-center">
+                  <ListGroupItem key={coin} className="d-flex justify-content-between align-items-center">
                     <div className="coin-info d-flex align-items-center">
-                        <span>{coin}</span>
+                      <span>{coin}</span>
                     </div>
-                    <div className="d-flex align-items-center">
+                    <div className="d-flex align-items-end">
+                      <div className="transact d-flex align-items-end">
                         <button onClick={() => initTransfer(coin)} className="btn btn-sm btn-success mr-1">+</button>
                         <button onClick={() => initTransfer(coin, true)} className="btn btn-sm btn-danger">-</button>
-                        <span className="mr-2">{amount}</span>
+                      </div>
+                      <span className="mr-2">{amount}</span>
+                      <span className="mr-2 trash"> <BsTrash onClick={() => delCoin(true, coin)} /> </span>
                     </div>
-                    </ListGroupItem>
+                  </ListGroupItem>
                 ))}
-                </ListGroup>
+              </ListGroup>
+
                 {showTrans ? (
                   <div>
                     <Modal isOpen={openModal} className="modal-container">
@@ -180,10 +224,17 @@ const Main = () => {
                       <div className="modal-header">{transferCoin} Transaction</div>
                       <label className='fw-bold'>{transText}</label>
                       <input type="number" className="modal-input" placeholder="Enter amount" value={transAmount} onChange={(e) => setTransAmount(parseFloat(e.target.value))} />
-                      <button className="modal-button" onClick={() => transaction(transferCoin, transAmount)}>Transfer</button>
+                      <button className="modal-button mb-2" onClick={() => transaction(transferCoin, transAmount)}>Transfer</button>
                     </Modal>
                   </div>
                   ) : null}
+
+                    <Modal isOpen={openTrash} className="modal-container" ariaHideApp={false}>
+                      <button className="modal-close-btn" onClick={() => closeSure()}>X</button>
+                      <p>Remove {deleteCoin} From Portfolio?</p>
+                      <button className="modal-button mb-2" onClick={() => handleSure() } >Remove {deleteCoin}</button>
+                    </Modal>
+
             </div>
           ) : (
             <div>
